1535c05dd6b72a503a5188bd9c182020e9e42c5e
SDBrett <brett008@gmail.com>
2018-01-17 09:26:14 +1100

    Set site title to appear on all pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,223 +1,124 @@
-// Define variables.
-var autoprefixer = require('autoprefixer');
-var browserSync  = require('browser-sync').create();
-var cleancss     = require('gulp-clean-css');
-var concat       = require('gulp-concat');
-var del          = require('del');
-var gulp         = require('gulp');
-var gutil        = require('gulp-util');
-var imagemin     = require('gulp-imagemin');
-var jshint       = require('gulp-jshint');
-var notify       = require('gulp-notify');
-var postcss      = require('gulp-postcss');
-var rename       = require('gulp-rename');
-var run          = require('gulp-run');
-var runSequence  = require('run-sequence');
-var sass         = require('gulp-ruby-sass');
-var uglify       = require('gulp-uglify');
-var paths        = require('./gulp_config/paths');
+var config = require('./gulpconfig.json');
+var	gulp = require('gulp');
+var	shell = require('gulp-shell');
+var cloudflare = require('gulp-cloudflare');
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
+var uncss = require('gulp-uncss');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var jpegtran = require('imagemin-jpegtran');
+var gifsicle = require('imagemin-gifsicle');
+var replace = require('gulp-replace');
+var fs = require('fs');
+var download = require('gulp-download');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
 
-// Uses Sass compiler to process styles, adds vendor prefixes, minifies, then
-// outputs file to the appropriate location.
-gulp.task('build:styles', function() {
-    return sass(paths.sassFiles + '/main.scss', {
-        style: 'compressed',
-        trace: true,
-        loadPath: [paths.sassFiles]
-    }).pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-        .pipe(cleancss())
-        .pipe(gulp.dest(paths.jekyllCssFiles))
-        .pipe(gulp.dest(paths.siteCssFiles))
-        .pipe(browserSync.stream())
-        .on('error', gutil.log);
+
+gulp.task('minify-css', () => {
+	return gulp.src('_site/assets/**/*.css')
+	  .pipe(cleanCSS({compatibility: 'ie8'}))
+	  .pipe(gulp.dest('_site/assets'));
+  });
+
+gulp.task('minify-html', function() {
+    return gulp.src('_site/**/*.html')
+      .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+      .pipe(gulp.dest('_site'));
+  });
+
+gulp.task('jekyll', function() {
+	return gulp.src('index.html', { read: false })
+		.pipe(shell([
+			'jekyll build'
+		]));
 });
 
-gulp.task('clean:styles', function(callback) {
-    del([paths.jekyllCssFiles + 'main.css', paths.siteCssFiles + 'main.css']);
-    callback();
+gulp.task('optimize-images', function () {
+	return gulp.src(['_site/**/*.jpg', '_site/**/*.jpeg', '_site/**/*.gif', '_site/**/*.png'])
+		.pipe(imagemin({
+			progressive: false,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant(), jpegtran(), gifsicle()]
+		}))
+		.pipe(gulp.dest('_site'));
 });
 
-// Concatenates and uglifies global JS files and outputs result to the
-// appropriate location.
-gulp.task('build:scripts:global', function() {
-    return gulp.src([
-        paths.jsFiles + '/global/lib' + paths.jsPattern,
-        paths.jsFiles + '/global/*.js'
-    ])
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.jekyllJsFiles))
-        .pipe(gulp.dest(paths.siteJsFiles))
-        .on('error', gutil.log);
+gulp.task('optimize-css', function() {
+   return gulp.src('_site/assets/css/**/*.css')
+	   .pipe(autoprefixer())
+	   .pipe(uncss({
+		   html: ['_site/**/*.html'],
+		   ignore: []
+	   }))
+	   .pipe(cleanCSS({keepBreaks: false}))
+	   .pipe(gulp.dest('_site/assets/css'));
 });
 
-gulp.task('clean:scripts', function(callback) {
-    del([paths.jekyllJsFiles + 'main.js', paths.siteJsFiles + 'main.js']);
-    callback();
+gulp.task('optimize-html', function() {
+	return gulp.src('_site/**/*.html')
+		.pipe(htmlmin({
+			quotes: true
+		}))
+		.pipe(replace(/<link href=\"\/css\/main.css\"[^>]*>/, function(s) {
+			var style = fs.readFileSync('_site/assets/css/main.css', 'utf8');
+			return '<style>\n' + style + '\n</style>';
+		}))
+		.pipe(gulp.dest('_site/'));
 });
 
-// Concatenates and uglifies leaflet JS files and outputs result to the
-// appropriate location.
-gulp.task('build:scripts:leaflet', function() {
-    return gulp.src([
-        paths.jsFiles + '/leaflet/leaflet.js',
-        paths.jsFiles + '/leaflet/leaflet-providers.js'
-    ])
-        .pipe(concat('leaflet.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.jekyllJsFiles))
-        .pipe(gulp.dest(paths.siteJsFiles))
-        .on('error', gutil.log);
+gulp.task('fetch-newest-analytics', function() {
+	return download('https://www.google-analytics.com/analytics.js')
+    	.pipe(gulp.dest('assets/'));
 });
 
-gulp.task('clean:scripts:leaflet', function(callback) {
-    del([paths.jekyllJsFiles + 'leaflet.js', paths.siteJsFiles + 'leaflet.js']);
-    callback();
+gulp.task('rsync-files', function() {
+	return gulp.src('index.html', { read: false })
+		.pipe(shell([
+			'cd _site && rsync -az --delete . ' + config.remoteServer + ':' + config.remotePath
+		]));
 });
 
-// Build all scripts.
-gulp.task('build:scripts', ['build:scripts:global', 'build:scripts:leaflet']);
-
-// Optimizes and copies image files.
-gulp.task('build:images', function() {
-    return gulp.src(paths.imageFilesGlob)
-        .pipe(imagemin())
-        .pipe(gulp.dest(paths.jekyllImageFiles))
-        .pipe(gulp.dest(paths.siteImageFiles))
-        .pipe(browserSync.stream());
+gulp.task('purge-cache', function() {
+	var options = {
+		token: config.cloudflareToken,
+		email: config.cloudflareEmail,
+		domain: config.cloudflareDomain
+	};
+ 
+	cloudflare(options);
 });
 
-gulp.task('clean:images', function(callback) {
-    del([paths.jekyllImageFiles, paths.siteImageFiles]);
-    callback();
+gulp.task('raw-deploy', function(callback) {
+	runSequence(
+		'jekyll',
+		'rsync-files',
+		'purge-cache',
+		callback
+	);
 });
 
-// Copies fonts.
-gulp.task('build:fonts', ['fontawesome']);
-
-// Places Font Awesome fonts in proper location.
-gulp.task('fontawesome', function() {
-    return gulp.src(paths.fontFiles + '/font-awesome/**.*')
-        .pipe(rename(function(path) {path.dirname = '';}))
-        .pipe(gulp.dest(paths.jekyllFontFiles))
-        .pipe(gulp.dest(paths.siteFontFiles))
-        .pipe(browserSync.stream())
-        .on('error', gutil.log);
+gulp.task('dry-run', function(callback) {
+	runSequence(
+		'fetch-newest-analytics',
+		'jekyll',
+		'optimize-images',
+		'optimize-css',
+		'minify-html',
+		callback
+	);
 });
 
-gulp.task('clean:fonts', function(callback) {
-    del([paths.jekyllFontFiles, paths.siteFontFiles]);
-    callback();
-});
-
-// Runs jekyll build command.
-gulp.task('build:jekyll', function() {
-    var shellCommand = 'bundle exec jekyll build --config _config.yml';
-
-    return gulp.src('')
-        .pipe(run(shellCommand))
-        .on('error', gutil.log);
-});
-
-// Runs jekyll build command using local config.
-gulp.task('build:jekyll:local', function() {
-    var shellCommand = 'bundle exec jekyll build --config _config.yml,_config.test.yml,_config.dev.yml';
-
-    return gulp.src('')
-        .pipe(run(shellCommand))
-        .on('error', gutil.log);
-});
-
-// Deletes the entire _site directory.
-gulp.task('clean:jekyll', function(callback) {
-    del(['_site']);
-    callback();
-});
-
-gulp.task('clean', ['clean:jekyll',
-    'clean:fonts',
-    'clean:images',
-    'clean:scripts',
-    'clean:styles']);
-
-// Builds site anew.
-gulp.task('build', function(callback) {
-    runSequence('clean',
-        'build:jekyll',
-        ['build:scripts', 'build:images', 'build:styles', 'build:fonts'],
-        callback);
-});
-
-// Builds site anew using local config.
-gulp.task('build:local', function(callback) {
-    runSequence('clean',
-        'build:jekyll:local',
-        ['build:scripts', 'build:images', 'build:styles', 'build:fonts'],
-        callback);
-});
-
-// Default Task: builds site.
-gulp.task('default', ['build']);
-
-// Special tasks for building and then reloading BrowserSync.
-gulp.task('build:jekyll:watch', ['build:jekyll:local'], function(callback) {
-    browserSync.reload();
-    callback();
-});
-
-gulp.task('build:scripts:watch', ['build:scripts'], function(callback) {
-    browserSync.reload();
-    callback();
-});
-
-// Static Server + watching files.
-// Note: passing anything besides hard-coded literal paths with globs doesn't
-// seem to work with gulp.watch().
-gulp.task('serve', ['build:local'], function() {
-
-    browserSync.init({
-        server: paths.siteDir,
-        ghostMode: false, // Toggle to mirror clicks, reloads etc. (performance)
-        logFileChanges: true,
-        logLevel: 'debug',
-        open: true        // Toggle to automatically open page when starting.
-    });
-
-    // Watch site settings.
-    gulp.watch(['_config.yml'], ['build:jekyll:watch']);
-
-    // Watch .scss files; changes are piped to browserSync.
-    gulp.watch('_assets/styles/**/*.scss', ['build:styles']);
-
-    // Watch .js files.
-    gulp.watch('_assets/js/**/*.js', ['build:scripts:watch']);
-
-    // Watch posts.
-    gulp.watch('_posts/**/*.+(md|markdown|MD)', ['build:jekyll:watch']);
-
-    // Watch drafts if --drafts flag was passed.
-    if (module.exports.drafts) {
-        gulp.watch('_drafts/*.+(md|markdown|MD)', ['build:jekyll:watch']);
-    }
-
-    // Watch html and markdown files.
-    gulp.watch(['**/*.+(html|md|markdown|MD)', '!_site/**/*.*'], ['build:jekyll:watch']);
-
-    // Watch RSS feed XML file.
-    gulp.watch('feed.xml', ['build:jekyll:watch']);
-
-    // Watch data files.
-    gulp.watch('_data/**.*+(yml|yaml|csv|json)', ['build:jekyll:watch']);
-
-    // Watch favicon.png.
-    gulp.watch('favicon.png', ['build:jekyll:watch']);
-});
-
-// Updates Ruby gems
-gulp.task('update:bundle', function() {
-    return gulp.src('')
-        .pipe(run('bundle install'))
-        .pipe(run('bundle update'))
-        .pipe(notify({ message: 'Bundle Update Complete' }))
-        .on('error', gutil.log);
+gulp.task('deploy', function(callback) {
+	runSequence(
+		'fetch-newest-analytics',
+		'jekyll',
+		'optimize-images',
+		'optimize-css',
+		'optimize-html',
+		'rsync-files',
+		'purge-cache',
+		callback
+	);
 });
